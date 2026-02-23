42e64ca5e9f4d46d4151198dc829bfdfa0ea4a55
BoLCHeViKe <bolchevike@gmail.com>
2026-02-23 13:33:07 +0000

    das

--- Vue/proyecto-vue-dwec/src/views/11-myList.vue
+++ Vue/proyecto-vue-dwec/src/views/11-myList.vue
@@ -1,32 +1,30 @@
 <script setup>
-import { reactive } from 'vue';
-let asig="";
-const milista = reactive(["Cliente","Servidor"]);
+import { reactive } from 'vue'
+let asig = ''
+const milista = reactive(['Cliente', 'Servidor'])
 //Mi script
-function agregarAsig(){
-    if (asig.trim()!="") {
-        if (milista.findIndex((element)=> element.toLowerCase()===asig.toLowerCase())===-1) {
-            milista.push(asig);
-            asig="";        
-        }
+function agregarAsig() {
+  if (asig.trim() != '') {
+    if (milista.findIndex((element) => element.toLowerCase() === asig.toLowerCase()) === -1) {
+      milista.push(asig)
+      asig = ''
     }
-
+  }
 }
 const resetList = () => {
-    milista.length = 0; 
-};
-
+  milista.length = 0
+}
 </script>
 <template>
-    <h2>Mi testeo de lista</h2>
-    <input v-model="asig" type="text" placeholder="Escribe asignatura"></input>
-    <button @click="agregarAsig()" >Guardar</button>
-    <button @click="resetList" >Limpiar Lista</button>
-    <ul>
-        <li v-for="(asignatura, index) in milista" :key="index">{{ index+1 }} - {{ asignatura }}</li>
-    </ul>
-    <!-- mi html -->
-    <!-- crear input y luego boton para añadir a la lista -->
+  <h2>Mi testeo de lista</h2>
+  <input v-model="asig" type="text" placeholder="Escribe asignatura" />
+  <button @click="agregarAsig()">Guardar</button>
+  <button @click="resetList">Limpiar Lista</button>
+  <ul>
+    <li v-for="(asignatura, index) in milista" :key="index">{{ index + 1 }} - {{ asignatura }}</li>
+  </ul>
+  <!-- mi html -->
+  <!-- crear input y luego boton para añadir a la lista -->
 </template>
 <style scoped>
 /* Mis estilos globales*/
